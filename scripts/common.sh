9bc87b06cf1ea4d6bea2930d057814ff0883f6c2
Mohammed amine blibech <160378998+med3633@users.noreply.github.com>
2024-04-23 17:45:00 +0100

    Update common.sh

--- scripts/common.sh
+++ scripts/common.sh
@@ -1,14 +1,14 @@
-#! /bin/bash
+#!/bin/bash
 
 # Variable Declaration
-
 KUBERNETES_VERSION="1.23.4-00"
 
-# disable swap 
+# Disable swap
 sudo swapoff -a
-# keeps the swaf off during reboot
+# Keeps the swap off during reboot
 sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
 
+# Install Docker dependencies and repository
 sudo apt-get update -y
 sudo apt-get install -y \
     apt-transport-https \
@@ -17,17 +17,17 @@ sudo apt-get install -y \
     gnupg \
     lsb-release
 
+# Add Docker GPG key and repository
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-
 echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
+# Install Docker CE
 sudo apt-get update -y
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 
-# Following configurations are recomended in the kubenetes documentation for Docker runtime. Please refer https://kubernetes.io/docs/setup/production-environment/container-runtimes/#docker
-
+# Configure Docker Runtime
 cat <<EOF | sudo tee /etc/docker/daemon.json
 {
   "exec-opts": ["native.cgroupdriver=systemd"],
@@ -45,16 +45,15 @@ sudo systemctl restart docker
 
 echo "Docker Runtime Configured Successfully"
 
-
-sudo apt-get update
+# Add Kubernetes GPG Key and APT Repository
+sudo apt-get update -y
 sudo apt-get install -y apt-transport-https ca-certificates curl
 sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
+# Install Kubernetes components
 sudo apt-get update -y
+sudo apt-get install -y kubelet==KUBERNETES_VERSION kubectl==KUBERNETES_VERSION kubeadm==KUBERNETES_VERSION
 
-sudo apt-get install -y kubelet={{ KUBERNETES_VERSION }} kubectl={{ KUBERNETES_VERSION }} kubeadm={{ KUBERNETES_VERSION }}
-
+# Mark Kubernetes packages on hold to prevent upgrades
 sudo apt-mark hold kubelet kubeadm kubectl
-
